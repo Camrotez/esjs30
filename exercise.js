cc470741fba28498003ded10dbba5323c777c1ac
Luciano <lunick997@gmail.com>
2023-06-21 12:01:32 +0200

    fix

--- exercise.js
+++ exercise.js
@@ -6,14 +6,20 @@ class Person {
     this.age = age;
   }
 
+  static fromJson(json){
+    const parsed = JSON.parse(json);
+    const {id, firstName, lastName, age} = parsed;
+    return new Person (id, firstName, lastName, age)
+  }
+
   toJson() {
     return JSON.stringify(this);
   }
 }
 
 const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
-// const developer = Person.fromJson(json);
-// console.log(developer);
+const developer = Person.fromJson(json);
+console.log(developer);
 
-const fromJson = JSON.parse(json)
-console.log(fromJson);
+// const fromJson = JSON.parse(json)
+// console.log(fromJson);
